ce3d1d03d101d2e20848b8f157217bb4b5604d87
PranjalGupta2199 <pranjalgupta2199@gmail.com>
2019-06-19 14:54:58 +0530

    react-dashboard: Replace Jumbotron with Carousel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import logo from './assets/logo.svg';
 import './styles/App.css';
-import {Navigation, Jtron} from './components/Dashboard';
+import {Navigation, CustomCarousel} from './components/Dashboard';
 
 function App() {
   return (
     <div>
       <Navigation/>
-      <Jtron/>
+      <CustomCarousel/>
     </div>
   );
 }
--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,53 +1,65 @@
-import React, { Component, Fragment } from "react";
-import { Navbar, Nav, Form, FormControl, Button, Container, NavDropdown } from "react-bootstrap";
-import Jumbotron from 'react-bootstrap/Jumbotron'
+import React, { Component } from "react";
+import { Navbar, Nav, Form, FormControl, Button, NavDropdown } from "react-bootstrap";
+import Img from '../assets/Img.jpg';
 import logo from '../assets/logo.svg';
+import Carousel from 'react-bootstrap/Carousel';
 
 
-
-export class Jtron extends Component{
+export class CustomCarousel extends Component{
   render(){
-    return (
-    <Jumbotron>
-      <h1>Seamless Map Service</h1>
-      <p>
-        This is a simple hero unit, a simple jumbotron-style component for calling
-        extra attention to featured content or information.
-      </p>
-      <p>
-        <Button variant="primary">Learn more</Button>
-      </p>
-    </Jumbotron>
+   return(
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={Img}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={Img}
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     )
   }
 }
 
-
 export class Navigation extends Component{
   
   render(){
-    const brand = <Navbar.Brand href = 'https://reactjs.org/'> Map - It ! </Navbar.Brand>
   
     return (
-      <Navbar bg="dark" variant="dark" expand="lg">
-        <Navbar.Brand href="#home">Map-It !</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-success">Search</Button>
-          </Form>
+      <Navbar bg="primary" variant="dark" expand="lg">
+      <Navbar.Brand href="#home">
+          <img
+            alt=""
+            src={logo}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />
+          {' Map-It !'}
+        </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="https://github.com/PranjalGupta2199/Map-It">Github</Nav.Link>
+          <Nav.Link href="https://bisag.gujarat.gov.in/">BISAG</Nav.Link>
+        </Nav>
+        <Button variant="warning">LOGIN</Button>
         </Navbar.Collapse>
       </Navbar>
     )
